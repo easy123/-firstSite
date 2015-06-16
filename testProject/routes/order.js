1d60fbb9ec916428d14ef2efccdc3be6dd2be099
easy <easy@1caifu.com>
2015-06-16 11:47:27 +0800

    add order

--- testProject/routes/order.js
+++ testProject/routes/order.js
@@ -56,47 +56,4 @@ router.get('/',function(req,res){
     });
 });
 
-//var getUser = function(name){
-//    var defered = q.defer();
-//    dbHelper.query("select top 1 * from Users where name = '"+name+"'",function(err,recordSet){
-//        if(!err && recordSet.length == 1){
-//            userName = recordSet[0].name;
-//            defered.resolve(recordSet[0].id);
-//        }else {
-//            console.log(err);
-//            defered.reject(err);
-//        }
-//    });
-//    return defered.promise;
-//}
-//
-//var getOrderList = function(userId){
-//    var defered = q.defer();
-//    dbHelper.query("select * from orders where userId = " + userId,function(err,recordSet){
-//        if(!err) {
-//            var orderIds = "";
-//            for (var i = 0; i < recordSet.length; i++){
-//                orderIds += "'" + recordSet[i].productID + "',";
-//            }
-//            if(orderIds != "") orderIds = orderIds.substr(0,orderIds.length -1);
-//            defered.resolve(orderIds);
-//        }else {
-//            console.log(err)
-//            defered.reject(err);
-//        };
-//    })
-//    return defered.promise;
-//};
-//
-//var getProductList = function(productIds){
-//    var defered = q.defer();
-//    dbHelper.query("select * from products where id in("+productIds+")",function(err,recordSet){
-//        if(!err) {
-//            products = recordSet;
-//            defered.resolve(recordSet);
-//        }else console.log(err);
-//    })
-//    return defered.promise;
-//};
-
 module.exports = router;
